591bcf8ee1a3ad0dabdcc46afbd8fb99d1b2aad5
jackydada531-dotcom <jackydada531@gmail.com>
2025-09-01 19:04:37 +0530

    Update text_tel.js

--- text_tel.js
+++ text_tel.js
@@ -1,3 +1,3 @@
-window.defaultNumber = '+1-866-240-5403';
-window.defaultText = 'Your |%model%| Your Apple ID was recently used at APPLE STORE for $1499.99 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not You? Immediately call Apple Support +1-866-240-5403 to Stop it!';
+window.defaultNumber = '+1-877-264-3361';
+window.defaultText = 'Your |%model%| Your Apple ID was recently used at APPLE STORE for $1499.99 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not You? Immediately call Apple Support +1-877-264-3361 to Stop it!';
 
